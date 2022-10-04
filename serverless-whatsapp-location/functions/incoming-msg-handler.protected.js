19f408dd495afc746f4d01bff3dfeb253a67feaf
Gus Varallo <gustavo@varallo.me>
2022-10-04 15:42:07 -0300

    refactor: improve logic to sync ProfileName without location

--- serverless-whatsapp-location/functions/incoming-msg-handler.protected.js
+++ serverless-whatsapp-location/functions/incoming-msg-handler.protected.js
@@ -1,55 +1,80 @@
 exports.handler = async function (context, event, callback) {
-  const response = new Twilio.twiml.MessagingResponse();
-  const syncServiceSid = context.TWILIO_SYNC_SERVICE_SID || "default";
+  const response = new Twilio.twiml.MessagingResponse()
+  const syncServiceSid = context.TWILIO_SYNC_SERVICE_SID || 'default'
 
-  const { To, From } = event;
-  const { Latitude, Longitude, ProfileName, Address, Label } = event;
+  const { To, From } = event
+  const { Latitude, Longitude, ProfileName, Address, Label } = event
 
-  const twilioClient = context.getTwilioClient();
+  const twilioClient = context.getTwilioClient()
 
-  if (From.startsWith("whatsapp:") && Latitude && Longitude) {
-    const syncDocName = `${From}-${To}`;
-    const expirySeconds = 60 * 60 * 24 * 7; //expires in 7 days
-    const locationData = {
-      Latitude,
-      Longitude,
-      ProfileName: ProfileName || "",
-      Address: Address || "",
-      Label: Label || "",
-    };
+  if (From.startsWith('whatsapp:')) {
+    const hasLocation = !!(Latitude && Longitude)
+    const syncDocName = `${From}-${To}`
+    const expirySeconds = 60 * 60 * 24 * 7 //expires in 7 days
+    let whatsappData = {
+      ProfileName,
+      Address: Address || '',
+      Label: Label || ''
+    }
+    let existingDoc
+
+    if (hasLocation) {
+      whatsappData = { ...whatsappData, Latitude, Longitude }
+    }
 
     try {
       await twilioClient.sync
         .services(syncServiceSid)
-        .documents(syncDocName)
-        .update({
-          ttl: expirySeconds,
-          data: locationData,
-        });
+        .documents.list()
+        .then(documents => {
+          existingDoc = documents.filter(d => d.uniqueName === syncDocName)[0]
+        })
     } catch (error) {
-      console.error("add sync item");
-      await twilioClient.sync.services(syncServiceSid).documents.create({
-        uniqueName: syncDocName,
-        ttl: expirySeconds,
-        data: locationData,
-      });
+      console.log('Error while listing documents:')
+      console.log(error)
+    }
+
+    try {
+      if (existingDoc) {
+        const { sid } = existingDoc
+
+        if (hasLocation) {
+          await twilioClient.sync
+            .services(syncServiceSid)
+            .documents(sid)
+            .update({
+              ttl: expirySeconds,
+              data: whatsappData
+            })
+        }
+      } else {
+        await twilioClient.sync.services(syncServiceSid).documents.create({
+          uniqueName: syncDocName,
+          ttl: expirySeconds,
+          data: whatsappData
+        })
+      }
+    } catch (error) {
+      console.log('An error has occurred:')
+      console.log(error)
     }
   }
+
   console.log(
     context.WHATSAPP_SANDBOX_NUMBER,
     context.WHATSAPP_SANDBOX_HANDLER_URL
-  );
+  )
   if (
     To === `whatsapp:${context.WHATSAPP_SANDBOX_NUMBER}` &&
     context.WHATSAPP_SANDBOX_HANDLER_URL
   ) {
-    const redirectResponse = new Twilio.Response();
-    redirectResponse.setStatusCode(302);
+    const redirectResponse = new Twilio.Response()
+    redirectResponse.setStatusCode(302)
     redirectResponse.setHeaders({
-      Location: context.WHATSAPP_SANDBOX_HANDLER_URL,
-    });
-    return callback(null, redirectResponse);
+      Location: context.WHATSAPP_SANDBOX_HANDLER_URL
+    })
+    return callback(null, redirectResponse)
   }
 
-  callback(null, response);
-};
+  callback(null, response)
+}
